82fef457cdcf896aeac8e7291cab06d42fe2367f
Egon Willighagen <egon.willighagen@gmail.com>
2022-03-15 20:19:10 +0100

    Updated to Bacting 0.0.33

--- sparql/createGrlc.groovy
+++ sparql/createGrlc.groovy
@@ -2,8 +2,8 @@
 //
 // GPL v3
 
-@Grab(group='io.github.egonw.bacting', module='managers-rdf', version='0.0.10')
-@Grab(group='io.github.egonw.bacting', module='managers-ui', version='0.0.10')
+@Grab(group='io.github.egonw.bacting', module='managers-rdf', version='0.0.33')
+@Grab(group='io.github.egonw.bacting', module='managers-ui', version='0.0.33')
 
 bioclipse = new net.bioclipse.managers.BioclipseManager(".");
 
